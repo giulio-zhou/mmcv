f46943a95b0112b36b57c7fc19a04f8d691d3493
Giulio Zhou <giuliozhou8@gmail.com>
2019-02-20 14:12:32 -0500

    Fix parallel evaluation code

--- examples/test_cifar10.py
+++ examples/test_cifar10.py
@@ -1,6 +1,8 @@
 import argparse
+import pickle
 from argparse import ArgumentParser
 
+import torch
 from torch.nn.parallel import DataParallel, DistributedDataParallel
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
@@ -11,7 +13,6 @@ import resnet_cifar
 
 import mmcv
 from mmcv.runner import load_checkpoint, parallel_test, obj_from_dict
-from mmcv.parallel import scatter, collate, MMDataParallel
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Test CIFAR10 models')
@@ -30,8 +31,40 @@ def parse_args():
     return args
 
 def _data_func(data, device_id):
-    data = scatter(collate([data], samples_per_gpu=1), [device_id])[0]
-    return dict(x=data[0])
+    data = data[0].unsqueeze(0).cuda(device_id)
+    return dict(x=data)
+
+def single_test(model, data_loader, show=False):
+    model.eval()
+    results = []
+    dataset = data_loader.dataset
+    prog_bar = mmcv.ProgressBar(len(dataset))
+    for i, data in enumerate(data_loader):
+        data, label = data
+        with torch.no_grad():
+            result = model(**dict(x=data))
+        results.append((result, label))
+
+        batch_size = data.size(0)
+        for _ in range(batch_size):
+            prog_bar.update()
+    return results
+
+def accuracy(output, target, topk=(1, )):
+    """Computes the precision@k for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
 
 def main():
     args = parse_args()
@@ -64,11 +97,22 @@ def main():
     elif cfg.model == 'mobilenetv2':
         model_cls = mobilenetv2.MobileNetV2
 
+    # Need higher ulimit for data loaders.
+    import resource
+    rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
+    resource.setrlimit(resource.RLIMIT_NOFILE, (16384, rlimit[1]))
+
     if args.gpus == 1:
         model = DataParallel(model_cls(), device_ids=cfg.gpus).cuda()
         load_checkpoint(model, checkpoint)
         outputs = single_test(model, val_loader, args.show)
+        targets = torch.LongTensor([x[1] for x in outputs]).cuda()
+        outputs = torch.cat([x[0] for x in outputs])
+        with open(args.out, 'wb') as f:
+            pickle.dump(outputs, f)
     else:
+        # NOTE: Parallel inference requires the data to be explicitly swapped to
+        #       cpu (add a .cpu() call to the result in parallel_test.py).
         # model_args = cfg.model.copy()
         # model_args.update(train_cfg=None, test_cfg=cfg.test_cfg)
         outputs = parallel_test(
@@ -79,7 +123,12 @@ def main():
             _data_func,
             range(args.gpus),
             workers_per_gpu=args.proc_per_gpu)
-    print(outputs)
+        targets = torch.LongTensor([val_dataset[i][1]
+                                    for i in range(len(val_dataset))])
+        outputs = torch.cat(outputs).cpu()
+        with open(args.out, 'wb') as f:
+            pickle.dump(outputs, f)
+    print(accuracy(outputs, targets, topk=(1,)))
 
 if __name__ == '__main__':
     main()
